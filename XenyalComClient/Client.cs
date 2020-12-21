56890a3a8a04aceeeac26bb38ef887667c5efbad
yaya-nyan <Florian.Denker@live.de>
2020-12-21 14:48:30 +0100

    Initial Commit

---
+++ XenyalComClient/Client.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using YayaLib;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using XenyalComClient;
+using System.Text;
+using System.Collections.Generic;
+
+namespace XenyalComClient
+{
+    class Client
+    {
+        static List<string> MessageHistory = new List<string> { };
+        static string Username;
+        static string IP;
+        static int Port;
+        static Socket soc;
+        static bool connected = false;
+        static bool disconnecting = false;
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(System.IntPtr hWnd, int cmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("Kernel32")]
+        private static extern bool SetConsoleCtrlHandler(EventHandler handler, bool add);
+
+        private delegate bool EventHandler(CtrlType sig);
+        static EventHandler handler;
+
+        static void Main(string[] args)
+        {
+            IntPtr handle = GetConsoleWindow();
+            IntPtr sysMenu = GetSystemMenu(handle, false);
+            DeleteMenu(sysMenu, 0xF000, 0x00000000);
+            DeleteMenu(sysMenu, 0xF030, 0x00000000);
+            ShowWindow(handle, 1);
+            handler += new EventHandler(Handler);
+            SetConsoleCtrlHandler(handler, true);
+
+            Console.Title = "Xenyal Terminal";
+            Console.WindowHeight = 31;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.BufferWidth = Console.WindowWidth;
+            DrawHeader();
+
+            Login();
+        }
+
+        static void DrawHeader()
+        {
+            Console.CursorTop = 0;
+            for (int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write("#");
+            }
+            Text.WriteLineCenter("__    __   ____    __    _   __    __   _____      __   ");
+            Text.WriteLineCenter("\\ \\  / /  |  __|  |  \\  | |  \\ \\  / /  /  _  \\    |  |  ");
+            Text.WriteLineCenter(" \\ \\/ /   | |_    |   \\ | |   \\ \\/ /  /  / \\  \\   |  |  ");
+            Text.WriteLineCenter("  >  <    |  _|   | |\\ \\| |    |  |  |  |___|  |  |  |  ");
+            Text.WriteLineCenter(" / /\\ \\   | |__   | | \\   |    |  |  |   ___   |  |  |__");
+            Text.WriteLineCenter("/_/  \\_\\  |____|  |_|  \\__|    |__|  |__|   |__|  |_____|");
+            Text.WriteLineCenter("Communication Client");
+            for (int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine("\n");
+            Console.CursorTop = 29;
+            for (int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write("#"); 
+            }
+        }
+        
+        static void Login()
+        {
+            bool choosingName = true;
+            while (choosingName)
+            {
+                Text.ClearLines(9, 29);
+                Console.SetCursorPosition(50, 15);
+                Console.Write("Username: ");
+                Username = Console.ReadLine();
+                Text.ClearLine(15);
+                if (Username.Length > 10)
+                {
+                    Text.WriteLineCenter("Too long.", 15);
+                    Console.ReadKey();
+                    Username = string.Empty;
+                }
+                if (Username.Length == 0)
+                {
+                    Text.WriteLineCenter("Too short.", 15);
+                    Console.ReadKey();
+                    Username = string.Empty;
+                }
+                if (Username != string.Empty) choosingName = false;
+            }
+            Username = Username.Trim();
+            Text.WriteLineCenter("Name Accepted.", 15);
+            Console.ReadKey();
+            bool connecting = true;
+            while(connecting)
+            {
+                Text.ClearLines(10, 29);
+                Console.SetCursorPosition(50, 15);
+                Console.Write("Server IP: ");
+                IP = Console.ReadLine();
+                int.TryParse(IP.Split(':')[1], out Port);
+                IP = IP.Split(':')[0];
+                try
+                {
+                    IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                    IPAddress.TryParse(IP, out IPAddress ipAddress);
+                    //IPAddress ipAddress = ipHostInfo.AddressList[0];
+                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, Port);
+
+                    soc = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                    try
+                    {
+                        soc.Connect(remoteEP);
+
+                        Text.ClearLines(9, 29);
+                        Console.CursorTop = 9;
+                        Console.WriteLine($"Currently connected to {soc.RemoteEndPoint}");
+
+                        soc.Send(Encoding.ASCII.GetBytes($"{Username}<CON>"));
+                        connected = true;
+                        disconnecting = false;
+                        ReceiveDataAsync();
+                        while (connected)
+                        {
+                            string messageText = Console.ReadLine();
+                            Text.ClearLine(Console.CursorTop - 1);
+                            if (messageText.Length > 0)
+                            {
+                                
+                                if(messageText.StartsWith('/'))
+                                {
+                                    if (messageText == "/exit")
+                                    {
+                                        disconnecting = true;
+                                        soc.Send(Encoding.ASCII.GetBytes($"{Username}<DIS>"));
+                                        LogOut();
+                                        return;
+                                    }
+                                    soc.Send(Encoding.ASCII.GetBytes($"{Username}=>{messageText.Remove(0,1)}<CMD>"));
+                                }
+                                else
+                                {
+                                    soc.Send(Encoding.ASCII.GetBytes($"{Username} said {messageText}<MSG>"));
+                                }
+                            }
+                            else UpdateMessageDisplay();
+                        }
+                    }
+                    catch (ArgumentNullException ane)
+                    {
+                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                    }
+                    catch (SocketException se)
+                    {
+                        Console.WriteLine("SocketException : {0}", se.ToString());
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Console.ReadKey();
+            }
+            Console.ReadKey();
+        }
+
+        static void LogOut()
+        {
+            MessageHistory.Clear();
+            connected = false;
+            soc.Close();
+            Console.ReadKey();
+            Login();
+        }
+
+        static async void ReceiveDataAsync()
+        {
+            while (!disconnecting)
+            {
+                byte[] bytes = new byte[1024];
+                try
+                {
+                    int bytesRec = await soc.ReceiveAsync(new ArraySegment<byte>(bytes), SocketFlags.None);
+                    string dataRec = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                    if (dataRec.EndsWith("<KICK>"))
+                    {
+                        dataRec = dataRec.Replace("<KICK>", "");
+                        MessageHistory.Add(dataRec);
+                        UpdateMessageDisplay();
+                        LogOut();
+                        return;
+                    }
+
+                    string[] messagesRec = dataRec.Split("\n");
+                    if (messagesRec.Length > 0)
+                    {
+                        foreach (var item in messagesRec)
+                        {
+                            MessageHistory.Add(item);
+                        }   
+                    }
+                    else MessageHistory.Add(dataRec);
+                    UpdateMessageDisplay();
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+        
+        static void UpdateMessageDisplay()
+        {
+            Text.ClearLines(10, 29);
+            Console.CursorTop = 10;
+            for (int i = Math.Max(MessageHistory.Count - 18, 0); i < MessageHistory.Count; i++)
+            {
+                Console.WriteLine(MessageHistory[i]);
+            }
+            Console.Write(" > ");
+        }
+
+        enum CtrlType
+        {
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+        }
+
+        private static bool Handler(CtrlType sig)
+        {
+            switch (sig)
+            {
+                case CtrlType.CTRL_LOGOFF_EVENT:
+                case CtrlType.CTRL_SHUTDOWN_EVENT:
+                case CtrlType.CTRL_CLOSE_EVENT:
+                    soc.Send(Encoding.ASCII.GetBytes($"{Username}<DIS>"));
+                    return true;
+                case CtrlType.CTRL_C_EVENT:
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ XenyalComServer/Server.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+public class User
+{
+    public User(string name, Socket soc)
+    {
+        Name = name;
+        Soc = soc;
+    }
+
+    public string Name;
+    public Socket Soc;
+    public MessageListenerThread MessageThread;
+}
+
+public class MessageListenerThread
+{
+    public MessageListenerThread(User user)
+    {
+        User = user;
+    }
+
+    public bool Active = true;
+    public User User;
+
+    public async void WaitForMessages()
+    {
+        while(Active)
+        {
+            byte[] bytes = new Byte[1024];
+            Console.WriteLine($"Waiting for Message by {User.Name}...");
+            string data = null;
+
+            if (User != null)
+            {
+                if (User.Soc.Connected)
+                {
+                    int userbytesRec = User.Soc.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, userbytesRec);
+                    await XenyalServer.ProcessData(data);
+                }
+                else break;
+            }
+        }
+        Console.WriteLine($"{User.Name}'s Message Thread ended.");
+    }
+}
+
+public class XenyalServer
+{
+    public static int Port;
+    public static DataType dataType = DataType.None;
+    public static User[] Users = new User[5];
+    public static Socket[] socs = new Socket[6];
+    public static Socket newestSoc;
+    public static TcpListener listener;
+    public static byte[] bytes = new Byte[1024];
+
+    public static int Main(String[] args)
+    {
+        Console.Title = "Xenyal Server";
+
+        Console.Write("Port: ");
+        int.TryParse(Console.ReadLine(), out Port);
+
+        string name = (args.Length < 1) ? Dns.GetHostName() : args[0];
+        listener = new TcpListener(Dns.GetHostAddresses(name)[3], Port);
+        Console.WriteLine($"Room IP: {Dns.GetHostAddresses(name)[3]}");
+        Console.CursorVisible = false;
+        Start();
+        while (true) { }
+    }
+
+    public static void Start()
+    {
+        listener.Start();
+        AcceptConnections();
+        while(true)
+        {
+            if(Users.Length > 0)
+            {
+                foreach (var item in Users)
+                {
+                    if(item != null)
+                    {
+                        if (item.MessageThread == null)
+                        {
+                            MessageListenerThread mlt = new MessageListenerThread(item);
+
+                            Thread Messages = new Thread(new ThreadStart(mlt.WaitForMessages));
+                            item.MessageThread = mlt;
+                            Messages.Start();
+                            Console.WriteLine($"Created new Message Thread for {item.Name}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public static async void AcceptConnections()
+    {
+        while(true)
+        {
+            Console.WriteLine("Waiting for Connection...");
+            for (int i = 1; i < socs.Length; i++)
+            {
+                if (socs[i] == null)
+                {
+                    socs[i] = await listener.AcceptSocketAsync();
+                    string data = null;
+                    int bytesRec = socs[i].Receive(bytes);
+                    newestSoc = socs[i];
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    await ProcessData(data);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static Task ProcessData(string data)
+    {
+        while (true)
+        {
+            if (data.IndexOf("<LOG>") > -1)
+            {
+                dataType = DataType.Info;
+                data = data.Replace("<LOG>", "");
+                break;
+            }
+            else if (data.IndexOf("<MSG>") > -1)
+            {
+                dataType = DataType.Message;
+                data = data.Replace("<MSG>", "");
+                break;
+            }
+            else if (data.IndexOf("<CON>") > -1)
+            {
+                dataType = DataType.Connect;
+                data = data.Replace("<CON>", "");
+                break;
+            }
+            else if (data.IndexOf("<DIS>") > -1)
+            {
+                dataType = DataType.Disconnect;
+                data = data.Replace("<DIS>", "");
+                break;
+            }
+            else if (data.IndexOf("<CMD>") > -1)
+            {
+                dataType = DataType.Command;
+                data = data.Replace("<CMD>", "");
+                break;
+            }
+        }
+
+        switch (dataType)
+        {
+            case DataType.Info:
+                Console.WriteLine(data);
+                break;
+            case DataType.Message:
+                Console.WriteLine(data);
+                SendToAll(Encoding.ASCII.GetBytes($"({DateTime.Now.Hour}:{DateTime.Now.Minute}) {data}"));
+                break;
+            case DataType.Connect:
+                if(UserCount() >= 5)
+                {
+                    Console.WriteLine($"{data} failed to connect: Server Full.");
+                    newestSoc.Send(Encoding.ASCII.GetBytes($"The Server you tried to connect to is full.<KICK>"));
+                    newestSoc = null;
+                    break;
+                }
+                if (!UserExists(data))
+                {
+                    Console.WriteLine($"{data} connected. ({UserCount()}/5)");
+                    AddUser(data, newestSoc);
+                    newestSoc = null;
+                    SendToAll(Encoding.ASCII.GetBytes($"{data} connected. ({UserCount()}/5)"));
+                }
+                else
+                {
+                    Console.WriteLine($"{data} failed to connect: Name taken.");
+                    newestSoc.Send(Encoding.ASCII.GetBytes($"A User with the name {data} is already connected to this IP.<KICK>"));
+                    newestSoc = null;
+                }
+                break;
+            case DataType.Disconnect:
+                Console.WriteLine($"{data} disconnected.");
+                RemoveUser(data);
+                SendToAll(Encoding.ASCII.GetBytes($"{data} disconnected."));
+                break;
+            case DataType.Command:
+                string[] segments = data.Split("=>");
+                Console.WriteLine($"{segments[0]} issued command {segments[1]}.");
+                switch (segments[1])
+                {
+                    case "help":
+                        SendToUser(Encoding.ASCII.GetBytes("Available Commands:\n" +
+                            "- /help     = Displays all available commands.\n" +
+                            "- /userlist = Displays all connected Users.\n" +
+                            "- /exit     = Disconnects you."), segments[0]);
+                        break;
+                    case "userlist":
+                        int i = 0;
+                        string userList = string.Empty;
+                        foreach (var item in Users)
+                        {
+                            if (item != null)
+                            {
+                                if (!item.Soc.Connected)
+                                {
+                                    RemoveUser(item.Name);
+                                    Console.WriteLine($"User {item.Name} disconnected.");
+                                }
+                                else
+                                {
+                                    userList += $" {item.Name},";
+                                    i++;
+                                }
+                            }
+                        }
+                        if (userList.EndsWith(',')) userList = userList.Remove(userList.Length - 1, 1);
+                        SendToUser(Encoding.ASCII.GetBytes($"{i} Users Connected:{userList}"), segments[0]);
+                        break;
+                    default:
+                        SendToUser(Encoding.ASCII.GetBytes($"Invalid command \"{segments[1]}\""), segments[0]);
+                        break;
+                }
+                break;
+            default:
+                Console.WriteLine($"Received invalid data: {data}");
+                break;
+        }
+        return Task.CompletedTask;
+    }
+
+    public static int SendToAll(byte[] bytes)
+    {
+        int i = 0;
+        foreach (var item in Users)
+        {
+            if(item != null)
+            {
+                if(!item.Soc.Connected)
+                {
+                    RemoveUser(item.Name);
+                    Console.WriteLine($"User {item.Name} disconnected.");
+                }
+                else
+                {
+                    item.Soc.Send(bytes);
+                    i++;
+                }
+            }
+        }
+        Console.WriteLine($"Sent {bytes.Length} bytes to {i} Users");
+        return i;
+    }
+
+    public static int UserCount()
+    {
+        int i = 0;
+        foreach (var item in Users)
+        {
+            if (item != null)
+            {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static bool UserExists(string name)
+    {
+        foreach (var item in Users)
+        {
+            if (item != null)
+            {
+                if (item.Name == name)
+                {
+                    if (!item.Soc.Connected)
+                    {
+                        RemoveUser(item.Name);
+                        Console.WriteLine($"User {item.Name} disconnected.");
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public static bool SendToUser(byte[] bytes, string name)
+    {
+        foreach (var item in Users)
+        {
+            if (item != null)
+            {
+                if(item.Name == name)
+                {
+                    if (!item.Soc.Connected)
+                    {
+                        RemoveUser(item.Name);
+                        Console.WriteLine($"User {item.Name} disconnected.");
+                        return false;
+                    }
+                    else
+                    {
+                        item.Soc.Send(bytes);
+                        Console.WriteLine($"Sent {bytes.Length} bytes to User {item.Name}");
+                        return true;
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"User {name} not found.");
+        return false;
+    }
+
+    public static bool AddUser(string name, Socket soc)
+    {
+        int i = 0;
+        foreach (var item in Users)
+        {
+            if(item == null)
+            {
+                Console.WriteLine($"User{i} \"{name}\" connected.");
+                Users[i] = new User(name, soc);
+                return true;
+            }
+            i++;
+        }
+        Console.WriteLine($"User \"{name}\" couldn't connect. (No empty slots)");
+        return false;
+    }
+
+    public static bool RemoveUser(string name)
+    {
+        int i = 0;
+        foreach (var item in Users)
+        {
+            if(item != null)
+            {
+                if (item.Name == name)
+                {
+                    Console.WriteLine($"User \"{name}\" disconnected.");
+                    item.MessageThread.Active = false;
+                    item.Soc.Close();
+                    Users[i] = null;
+                    return true;
+                }
+            }
+            i++;
+        }
+        Console.WriteLine($"User \"{name}\" not found.");
+        return false;
+    }
+}
+
+public enum DataType
+{
+    None,
+    Info,
+    Connect,
+    Disconnect,
+    Message,
+    Command
+}
